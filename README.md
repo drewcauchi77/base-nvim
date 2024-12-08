4c0539283c6677e0971d636570bfa7299f1bf906
Andrew Cauchi <drewcauchi@gmail.com>
2024-12-08 23:21:48 +0100

    Added csharp LSP. Fixed lua LSP. Added ReadMe.

---
+++ README.md
@@ -0,0 +1,1 @@
+# How To Setup this NVIM configuration (Windows)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,10 +9,10 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { 
+                ensure_installed = {
                     "lua_ls",
                     "ts_ls",
-                    "omnisharp"
+                    "csharp_ls"
                 }
             })
         end
@@ -23,11 +23,11 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
-            lspconfig.omnisharp.setup({})
+            lspconfig.csharp_ls.setup({})
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end
     }
 }
